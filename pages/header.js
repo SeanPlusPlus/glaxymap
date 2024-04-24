70421c3eeafdfd314dfcf73c15fc8923c36d1ee2
Sean Stephenson <sean.m.stephenson@disney.com>
2024-04-24 11:26:36 -0700

    main: header

--- pages/header.js
+++ pages/header.js
@@ -1,17 +1,17 @@
 import Head from "next/head"
 
-// const SITE = "https://firstround-nfl.vercel.app/"
+const SITE = "https://glaxymap.vercel.app/"
 
 export default function Header() {
   return (
     <Head>
       <title>Galaxy Map</title>
-      <meta name="description" content="Map of the galaxy" />
+      <meta name="description" content="Interactive map of our galaxy" />
       <link rel="icon" href="/favicon.ico" />
-      {/* <meta property="og:image" content={`${SITE}logo.png`} />
+      <meta property="og:image" content={`${SITE}logo.png`} />
       <meta property="og:url" content={SITE} />
-      <meta property="og:site_name" content="First Round NFL" />
-      <meta property="og:type" content="website" /> */}
+      <meta property="og:site_name" content="Galaxy Map" />
+      <meta property="og:type" content="website" />
     </Head>
   )
 }
